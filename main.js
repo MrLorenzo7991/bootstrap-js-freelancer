7913d0715ed8be3c59fac8b233e4bcf07f54c2bf
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-29 13:04:52 +0200

    Aggiunta function calcolo prezzo

--- main.js
+++ main.js
@@ -3,15 +3,33 @@ const commissioneFrontend = 15.3;
 const commissioneAnalisi = 33.6;
 let codiciScontoValidi = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
 const sconto = 0.75;
-/*const submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", submitForm(e){
-    e.preventDefault();
-})*/
-    
+
 
 function submitForm(event){
-    event.preventeDefault();
+    event.preventDefault();
     let oreRichieste = document.getElementById("hoursRequest").value;
-    let tipoDiLavoro = document.getElementById("typeOfWork").value
+    let tipoDiLavoro = document.getElementById("typeOfWork").value;
+    let codiceSconto = document.getElementById("discountCode").value;
+    let flagSconto = checkSconto(codiceSconto);
 
+}
+//check codice sconto
+function checkSconto(codice){
+    let flagSconto = false
+    for(i=0, i < codiciScontoValidi.length, i++){
+        if(codice == codiciScontoValidi(i)){
+            flagSconto = true;
+            break
+        }
+    }
+    return(flagSconto);
+}
+
+//Calcolo prezzo
+function calcoloPrezzo(ore, commissione, dirittoSconto){
+    let prezzofinale = ore*commissione;
+    if(dirittoSconto){
+        prezzofinale = prezzofinale*sconto;
+    }
+    return(prezzofinale)
 }
