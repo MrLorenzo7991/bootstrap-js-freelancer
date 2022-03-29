18d69440f207a3950120a62f57ed40076293c9c1
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-29 12:39:53 +0200

    Inzio parte JS

---
+++ main.js
@@ -0,0 +1,16 @@
+const commissioneBackend = 20.5;
+const commissioneFrontend = 15.3;
+const commissioneAnalisi = 33.6;
+let codiciScontoValidi = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+const sconto = 0.75;
+/*const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", submitForm(e){
+    e.preventDefault();
+})*/
+    
+
+function submitForm(event){
+    event.preventeDefault();
+    let oreRichieste = document.getElementById("")
+
+}
